7a0e47b7de06011a528d96273d8801bbe757f3c0
Roger E Randall II <roger.randall@gmail.com>
2018-04-17 21:02:52 -0500

    add initial queries to find driver/constructor total points

--- lib/f1/stats/stats.ex
+++ lib/f1/stats/stats.ex
@@ -1,16 +1,67 @@
 defmodule F1.Stats do
   import Ecto.Query
   alias F1.Repo
-  alias F1.Stats.Constructor
 
   def list_all(data) do
     data
-    |> Repo.all
+    |> Repo.all()
   end
 
   def total_number_of(data) do
     data
-    |> Repo.all
-    |> Enum.count
+    |> Repo.all()
+    |> Enum.count()
+  end
+
+  def total_points(:driver) do
+    query =
+      from(
+        r in F1.Stats.Result,
+        join: d in F1.Stats.Driver,
+        where: r.driverId == d.driverId
+      )
+    query =
+      from(
+        [r, d] in query,
+        join: race in F1.Stats.Race,
+        where: r.raceId == race.raceId
+      )
+    query =
+      from(
+        [r, d, race] in query,
+        where: race.round >= 1,
+        select: %{
+          driver: d.surname,
+          points: sum(r.points)},
+        group_by: d.driverId,
+        order_by: [desc: sum(r.points)]
+      )
+    Repo.all(query)
+  end
+
+  def total_points(:constructor) do
+    query =
+      from(
+        r in F1.Stats.Result,
+        join: c in F1.Stats.Constructor,
+        where: r.constructorId == c.constructorId
+      )
+    query =
+      from(
+        [r, c] in query,
+        join: race in F1.Stats.Race,
+        where: r.raceId == race.raceId
+      )
+    query =
+      from(
+        [r, c, race] in query,
+        where: race.round >= 1,
+        select: %{
+          constructor: c.name,
+          points: sum(r.points)},
+        group_by: c.constructorId,
+        order_by: [desc: sum(r.points)]
+      )
+    Repo.all(query)
   end
 end
