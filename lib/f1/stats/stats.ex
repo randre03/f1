46b366bd4666ba20a35a9e21e4d646c98b817c34
Roger E Randall II <roger.randall@gmail.com>
2018-04-17 21:15:51 -0500

    reduce size of query code

--- lib/f1/stats/stats.ex
+++ lib/f1/stats/stats.ex
@@ -2,66 +2,32 @@ defmodule F1.Stats do
   import Ecto.Query
   alias F1.Repo
 
-  def list_all(data) do
-    data
-    |> Repo.all()
-  end
-
-  def total_number_of(data) do
-    data
-    |> Repo.all()
-    |> Enum.count()
-  end
-
   def total_points(:driver) do
-    query =
       from(
         r in F1.Stats.Result,
         join: d in F1.Stats.Driver,
-        where: r.driverId == d.driverId
-      )
-    query =
-      from(
-        [r, d] in query,
+        where: r.driverId == d.driverId,
         join: race in F1.Stats.Race,
-        where: r.raceId == race.raceId
-      )
-    query =
-      from(
-        [r, d, race] in query,
+        where: r.raceId == race.raceId,
         where: race.round >= 1,
-        select: %{
-          driver: d.surname,
-          points: sum(r.points)},
+        select: %{driver: d.surname, points: sum(r.points)},
         group_by: d.driverId,
         order_by: [desc: sum(r.points)]
       )
-    Repo.all(query)
+    |> Repo.all
   end
 
   def total_points(:constructor) do
-    query =
-      from(
-        r in F1.Stats.Result,
-        join: c in F1.Stats.Constructor,
-        where: r.constructorId == c.constructorId
-      )
-    query =
-      from(
-        [r, c] in query,
-        join: race in F1.Stats.Race,
-        where: r.raceId == race.raceId
-      )
-    query =
-      from(
-        [r, c, race] in query,
-        where: race.round >= 1,
-        select: %{
-          constructor: c.name,
-          points: sum(r.points)},
-        group_by: c.constructorId,
-        order_by: [desc: sum(r.points)]
-      )
-    Repo.all(query)
+    from(
+      r in F1.Stats.Result,
+      join: c in F1.Stats.Constructor,
+      where: r.constructorId == c.constructorId,
+      join: race in F1.Stats.Race,
+      where: r.raceId == race.raceId,
+      where: race.round >= 1,
+      select: %{constructor: c.name, points: sum(r.points)},
+      group_by: c.constructorId,
+      order_by: [desc: sum(r.points)])
+    |> Repo.all
   end
 end
